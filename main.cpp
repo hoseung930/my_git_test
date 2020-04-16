70138ceb832d156bf940546c795f2b446f465159
hoseung930 <21500627@handong.edu>
2020-04-16 02:58:09 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "sorting.h"
-
 int sorted[10];
-
+int sorted1[10];
+int num = 0;
 int main(){
 
+    for(int i = 0 ; i<10; i++){
+    cin >> num;
+    sorted1[i] = num;
+    }
+    int N = sizeof(sorted)/sizeof(sorted[0]);
+    merge_sort(sorted1,0,9);
+    for(int i = 0; i<N; i++){
+    cout<< sorted[i]<< " ";
+
+    }
     return 0;
 }
